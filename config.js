db591a008da4355da8dde8f9398d7a75d1fa7bed
hrvworld <hrv1302@gmail.com>
2026-02-12 13:06:20 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Tushy's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Tush",
+    valentineName: "Tushy",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
